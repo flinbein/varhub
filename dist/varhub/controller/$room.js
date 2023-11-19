9a817e92edc8cff4908fe808ea8138b55fca4650
dpohvar <dpohvar@gmail.com>
2023-11-19 20:22:52 +0200

    test husky x

---
+++ dist/varhub/controller/$room.js
@@ -0,0 +1,11 @@
+// @ts-nocheck
+import { target, clientMap, send, broadcast, close } from "varhub:inner";
+export default Object.freeze({
+    getClients: () => [...clientMap.values()],
+    getClientById: (id) => clientMap.get(id),
+    addEventListener: target.addEventListener.bind(target),
+    removeEventListener: target.removeEventListener.bind(target),
+    send,
+    broadcast,
+    close
+});
