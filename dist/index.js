477c5abb90b31c268f5cf6d88c114ec7bb5db4b2
dpohvar <dpohvar@gmail.com>
2024-03-24 22:35:02 +0200

    export controllers

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,6 @@ export { Hub } from "./varhub/Hub.js";
 export { Room } from "./varhub/Room.js";
 export { Connection } from "./varhub/Connection.js";
 export { default as TypedEventEmitter } from "./utils/TypedEventEmitter.js";
+export { ApiHelperController } from "./controllers/ApiHelperController.js";
+export { PlayerController } from "./controllers/PlayerController.js";
+export { RPCController } from "./controllers/RPCController.js";
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@ export { Hub } from  "./varhub/Hub.js"
 export { Room } from  "./varhub/Room.js"
 export { Connection } from "./varhub/Connection.js"
 export { default as TypedEventEmitter } from  "./utils/TypedEventEmitter.js"
+export { ApiHelper, ApiHelperController } from  "./controllers/ApiHelperController.js"
+export { PlayerController, Player } from "./controllers/PlayerController.js"
+export { RPCController } from "./controllers/RPCController.js"
 
